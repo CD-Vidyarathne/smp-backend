723ded173b043b4b6f08a99110812cb0f04ee338
CD-Vidyarathne <chamindudvidyarathne@gmail.com>
2024-03-03 20:04:19 +0530

    first commit

---
+++ feedpost/src/main/java/com/eadcw/feedpost/FeedpostApplication.java
@@ -0,0 +1,13 @@
+package com.eadcw.feedpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedpostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedpostApplication.class, args);
+	}
+
+}
